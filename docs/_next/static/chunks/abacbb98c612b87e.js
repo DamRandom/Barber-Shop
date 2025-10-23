b4329ef476712025d3cc9a422ae9540d9ace716d
Damian Brito <britohdamian@gmail.com>
2025-10-23 14:41:38 -0500

    renamed out to docs

---
+++ docs/_next/static/chunks/abacbb98c612b87e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/7e06268b1063f847.js",
+  "static/chunks/e343e01607d9b28e.js",
+  "static/chunks/turbopack-173877bc5292ad7b.js"
+])
---
+++ docs/_next/static/chunks/ae35e5dc50b76fef.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/9e6add38d6188caa.js",
+  "static/chunks/e343e01607d9b28e.js",
+  "static/chunks/turbopack-5bbbb80afa2c47b3.js"
+])
